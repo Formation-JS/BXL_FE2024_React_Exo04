1bc77a51a3028efa00a8b94cc53dfc75a59e8864
Aurélien St <aurelien.strimelle@bstorm.be>
2024-05-24 13:28:10 +0200

    Component Calculatrice 🧂

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Calculatrice from './components/Calculatrice/Calculatrice';
 
 function App() {
 
   return (
     <>
       <h1>Exo 03 - Les formulaires</h1>
+      <Calculatrice />
     </>
   )
 }
---
+++ src/components/Calculatrice/Calculatrice.jsx
@@ -0,0 +1,34 @@
+const Calculatrice = () => {
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="">Nombre 1 : </label>
+                <input id="" type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Operation : </label>
+                <select id="">
+                    <options></options>
+                    <options></options>
+                    <options></options>
+                    <options></options>
+                    <options></options>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="">Nombre 2 : </label>
+                <input id="" type="text" />
+            </div>
+            <div>
+                <button type="submit">Calculer</button>
+            </div>
+            <div>
+                <label htmlFor="">Resultat : </label>
+                <input id="" type="text" />
+            </div>
+        </form>
+    );
+};
+
+export default Calculatrice;
